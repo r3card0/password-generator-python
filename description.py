726871fb8f559887fb8cdbd09e85702ed232595d
r3card0 <ricardo.rdz@pm.me>
2022-11-12 20:54:53 -0600

    description of the program

---
+++ description.py
@@ -0,0 +1,13 @@
+def description():
+    program_description ="""
+    This program generates a password taking characteres from a set of lists
+    """
+    return program_description
+
+
+def run():
+    print(description())
+
+
+if __name__ == "__main__":
+    run()

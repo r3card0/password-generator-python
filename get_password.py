064bd7e767d16affad13b6686cad0a1640bced2d
r3card0 <ricardo.rdz@pm.me>
2022-11-12 20:56:30 -0600

    review it

---
+++ get_password.py
@@ -0,0 +1,14 @@
+def get_character_lists():
+    upperc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    lowerc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    symbols = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+    chars = upperc + lowerc + numbers + symbols
+    return chars
+
+def run():
+    print(get_character_lists())
+
+
+if __name__ == "__main__":
+    run()
